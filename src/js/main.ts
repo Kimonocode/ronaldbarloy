72c63c9acab88f5e2445db096c659d4d57a780a4
kimonocode <ronald.github.dev@gmail.com>
2023-04-13 21:09:52 +0200

    design progress in desktop screen

--- src/js/main.ts
+++ src/js/main.ts
@@ -2,6 +2,7 @@ import 'normalize.css';
 import '../css/index.scss';
 import mobileIland1Img from '../assets/iland1-mobile.png';
 import mobileIland2Img from '../assets/iland2-mobile.png';
+import heroBanner from '../assets/herobanner.png';
 import workItem1Img from '../assets/work1.png';
 import workItem2Img from '../assets/work2.png';
 import workItem3Img from '../assets/work3.png';
@@ -9,9 +10,7 @@ import folderIcon from '../assets/folder.svg';
 
 const pictures = document.getElementsByTagName('img');
 
-/* const mobileIland1 = document.getElementById('mobileIland1')!;
-const mobileIland2 = document.getElementById('mobileIland2')!; */
-
+pictures.namedItem('heroBanner')!.src = heroBanner;
 pictures.namedItem('mobileIland1Img')!.src = mobileIland1Img;
 pictures.namedItem('mobileIland2Img')!.src = mobileIland2Img;
 pictures.namedItem('workItem1')!.src = workItem1Img;
